4453f38c0782be5b892106518f5164ea8a465de9
lesli <ug@fontbonne.edu>
2023-02-12 23:01:11 -0800

    craeting search bar

---
+++ lib/searchingview.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class SearchingPageView extends StatelessWidget {
+  const SearchingPageView({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Text(
+          "This is Searching Page View",
+        ),
+      ],
+    );
+  }
+}
